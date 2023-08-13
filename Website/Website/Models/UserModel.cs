db62a90ddc48918c9bb6b2ac76ed1d9734afb25f
Egor <Yehor.Chaplyhin@cs.khpi.edu.ua>
2023-08-13 03:07:29 +0200

    update 2.0

---
+++ Website/Website/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Website.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool Admin { get; set; }
+        public bool Manufacturer { get; set; }
+    }
+}
