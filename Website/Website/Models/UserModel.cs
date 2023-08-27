3f1243b5e30e286211a4d493ca5a5155deb835fb
Egor <Yehor.Chaplyhin@cs.khpi.edu.ua>
2023-08-27 02:39:34 +0200

    .

--- Website/Website/Models/UserModel.cs
+++ Website/Website/Models/UserModel.cs
@@ -7,8 +7,26 @@
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Login { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public bool Admin { get; set; }
         public bool Manufacturer { get; set; }
+
+        public string EmailGet()
+        {
+            return Email;
+        }
+
+        public bool PasswordRemove(string password1, string password2)
+        {
+            if (password1 == password2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
